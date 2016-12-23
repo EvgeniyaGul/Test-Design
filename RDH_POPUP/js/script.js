38df4c096a9d6d867c7ae9fd6ccea0057da80efd
EvgeniyaGul <evgeniya@yandex.ru>
2016-12-23 19:59:06 +0300

    p

--- RDH_POPUP/js/script.js
+++ RDH_POPUP/js/script.js
@@ -11,15 +11,15 @@ $(window).load(function() {
 		if ($(window).width() > 960)
 		{
             $('.main-posts').each(function(){
-                var rightblock = $(this).find('.right-block .sticked');
+                var rightblock = $(this).find('.rightblock .sticked');
                 var rightblock_top = rightblock.offset().top;
                 var rightblock_bottom = rightblock_top+rightblock.height();
                 
 				if (rightblock.length)
 				{
-					/*var top_position = 25;
-					я убрала*/
-					var bottom_position = 0;//добавлено
+					var top_position = 25;
+					/*я убрала*/
+					
 					var window_top = $(window).scrollTop() /*+ top_position;*/
 					var window_bottom = $(window).scrollTop()+$(window).height();
 					
@@ -30,15 +30,15 @@ $(window).load(function() {
 					var scrolled = $(window).scrollTop()//добавлено
 					
 
-					//console.log('w_height = '+w_height); //добавлено
-					//console.log('block height = '+ rightblock.height()); //добавлено
-					//console.log('scrolled = '+ scrolled); //добавлено
+					console.log('w_height = '+w_height); //добавлено
+					console.log('block height = '+ rightblock.height()); //добавлено
+					console.log('scrolled = '+ scrolled); //добавлено
 
-                    //console.log('el_top = '+el_top+'; el_bottom='+el_bottom);
-					//console.log('rightblock = '+rightblock_top + '; rightblock_bottom = '+rightblock_bottom);
-                    //console.log('window_top = '+window_top+'; window_bottom='+window_bottom)
+                    console.log('el_top = '+el_top+'; el_bottom='+el_bottom);
+					console.log('rightblock = '+rightblock_top + '; rightblock_bottom = '+rightblock_bottom);
+                    console.log('window_top = '+window_top+'; window_bottom='+window_bottom)
 					
-					/*if (el_top + top_position > window_top)
+					if (el_top + top_position > window_top)
 					{
 						rightblock.css({position:'absolute', 'top': 0, 'right':'15px', 'left':'auto'})
 						console.log('блок ушел вниз');
@@ -54,15 +54,50 @@ $(window).load(function() {
                     //if (el_top <= window_top && el_bottom - rightblock.height() >= window_top) //я убрала
 					//if (rightblock.height() - window_top <= w_height)//я убрала свое
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 					if ( rightblock.height() - window_top <= w_height )
 					{
 						//var container = rightblock.parents('.container')||$('.container:first'); //я убрала
 						//var right_position = container.offset().left + container.width() - rightblock.width()+15;//я убрала
 						
 						//rightblock.css({position:'fixed', top: top_position, 'left':right_position, 'right':'auto'})
-						rightblock.css({position:'fixed', bottom: bottom_position, 'right':'auto'}) //добавлено
+						rightblock.css({position:'fixed', bottom: 0, 'right':'auto'}) //добавлено
 						
 						console.log('блок в окне')
+					}
+
+					if ( el_bottom - window_top <= w_height )
+					{
+						//var container = rightblock.parents('.container')||$('.container:first'); //я убрала
+						//var right_position = container.offset().left + container.width() - rightblock.width()+15;//я убрала
+						
+						//rightblock.css({position:'fixed', top: top_position, 'left':right_position, 'right':'auto'})
+						rightblock.css({position:'absolute', top: 350, 'right':'auto'}) //добавлено
+						
+						console.log('блок ушел вверх')
+					}
+
+					if (  rightblock.height() - window_top > w_height )
+					{
+						//var container = rightblock.parents('.container')||$('.container:first'); //я убрала
+						//var right_position = container.offset().left + container.width() - rightblock.width()+15;//я убрала
+						
+						//rightblock.css({position:'fixed', top: top_position, 'left':right_position, 'right':'auto'})
+						rightblock.css({position:'absolute', top: 0, 'right':'auto'}) //добавлено
+						
+						console.log('блок ушел вниз')
 					}
 //добавлено
 					/*if (el_bottom - rightblock.height() < window_bottom)
@@ -75,11 +110,7 @@ $(window).load(function() {
 						
 						console.log('блок ушел наверх')
 					}*/
-					/*if ((rightblock.css({position:'fixed'})) && (el_bottom - window_top >= w_height))
-					{
-						rightblock.css({position:'absolute', 'top': 0, 'right':'15px', 'left':'auto'})
-						console.log('блок ушел вниз');
-					}*/
+					
 
                 }
             })

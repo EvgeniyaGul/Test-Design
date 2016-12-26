3eb81dcb5c21756af1bb17c7f385149710f6f327
EvgeniyaGul <evgeniya@yandex.ru>
2016-12-26 18:26:40 +0300

    p

--- RDH_POPUP/js/js.js
+++ RDH_POPUP/js/js.js
@@ -27,7 +27,7 @@ $(function() {
                 
 				if (rightblock.length)
 				{
-					var window_top = $(window).scrollTop() /*+ top_position;*/
+					var window_top = $(window).scrollTop()
 					var window_bottom = $(window).scrollTop() + $(window).height();
 					
 					var el_top = $(this).offset().top;
@@ -37,20 +37,26 @@ $(function() {
 					console.log('el_top = '+el_top+'; el_bottom='+el_bottom);
 					console.log('rightblock = '+rightblock_top + '; rightblock_bottom = '+rightblock_bottom);
                     console.log('window_top = '+window_top+'; window_bottom='+window_bottom)
+
+					console.log('w_height = ' + w_height);
+
 					
 					if ( rightblock.height() - window_top + el_top <= w_height )
 					{
 						var container = rightblock.parents('.container')||$('.container:first');
 						var right_position = container.offset().left + container.width() - rightblock.width() + 15;
-						var top_fixed = rightblock_top - window_top; 
+						//var top_fixed = rightblock_top - window_top;
+						//var top_fixed = window_bottom - window_top - rightblock_top; 
 
-						rightblock.css({position:'fixed', top: top_fixed, 'left': right_position, 'right':'auto'}) 
+						rightblock.css({position:'fixed', top: window_top, 'left': right_position, 'right':'auto'}) 
 						console.log('блок в окне')
-					}
+					} 
 
-					if ( el_bottom - window_top <= w_height )
+					//if ( el_bottom - window_top <= w_height )
+					if ( rightblock_top - w_height >= rightblock.height() )
 					{
 						var top_absolute = el_bottom - el_top - rightblock.height(); 
+						//var top_absolute = rightblock_top - window_top - el_top;
 						
 						rightblock.css({position:'absolute', top: top_absolute, 'right': 15, 'left':'auto'}) //добавлено
 						console.log('блок ушел вверх')
@@ -59,8 +65,19 @@ $(function() {
 					if (  rightblock.height() - window_top > w_height )
 					{
 						rightblock.css({position:'absolute', top: 0, 'right': 15, 'left':'auto'}) //добавлено
-						console.log('блок ушел вниз')
+						console.log('блок ушел с контентом')
 					}
+
+					//для высоты баннера меньше высоты окна
+
+					if ( rightblock.height() < w_height && (  el_bottom - window_top > w_height ))
+					{
+						rightblock.css({position:'absolute', top: window_top, 'right': 15, 'left':'auto'}) //добавлено
+						console.log('блок ушел с контентом_2')
+
+					}
+
+
 					
                 }
             })

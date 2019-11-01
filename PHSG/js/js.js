8693a6abb9d370f28cdfd9616a829dfaa4af175e
EvgeniyaGul <gulevgeniya@yandex.ru>
2019-11-01 23:36:27 +0100

    js.js added

--- PHSG/js/js.js
+++ PHSG/js/js.js
@@ -1,3 +1,45 @@
+// Carousel
 $('.carousel').carousel({
     pause: "false" /* Change to true to make it paused when your mouse cursor enter the background */
 });
+
+// Button scroll up
+$(function() {
+  $('.scrollup').click(function() {
+    $("html, body").animate({
+      scrollTop:0
+    },1000);
+  })
+});
+$(window).scroll(function() {
+  if ($(this).scrollTop()>200) {
+    $('.scrollup').fadeIn();
+  }
+  else {
+    $('.scrollup').fadeOut();
+  }
+});
+// Carousel-section
+$('#carousel-section').on('slide.bs.carousel', function (e) {
+    /*
+        CC 2.0 License Iatek LLC 2018 - Attribution required
+    */
+    var $e = $(e.relatedTarget);
+    var idx = $e.index();
+    var itemsPerSlide = 2;
+    var totalItems = $('.carousel-inner-section').children('.carousel-item').length;
+ 
+    if (idx >= totalItems-(itemsPerSlide-1)) {
+        var it = itemsPerSlide - (totalItems - idx);
+        for (var i=0; i<it; i++) {
+            // append slides to end
+            if (e.direction=="left") {
+                $('.carousel-inner-section').children('.carousel-item').eq(i).appendTo('#carousel-section > .carousel-inner');
+            }
+            else {
+                $('.carousel-inner-section').children('.carousel-item').eq(0).appendTo('#carousel-section > .carousel-inner');
+            }
+        }
+    }
+});
+
